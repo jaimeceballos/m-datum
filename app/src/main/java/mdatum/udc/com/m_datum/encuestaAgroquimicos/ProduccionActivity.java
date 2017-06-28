3aae30b7ddc9cb4c82f3becafc889eaa633438fa
Luis Torres <luistorresrw@hotmail.com>
2017-06-28 11:16:41 -0300

    Creacion de CultivoActivity

--- app/src/main/java/mdatum/udc/com/m_datum/encuestaAgroquimicos/ProduccionActivity.java
+++ app/src/main/java/mdatum/udc/com/m_datum/encuestaAgroquimicos/ProduccionActivity.java
@@ -2,6 +2,8 @@ package mdatum.udc.com.m_datum.encuestaAgroquimicos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RadioButton;
 
 import mdatum.udc.com.m_datum.R;
 
@@ -12,6 +14,13 @@ public class ProduccionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_produccion);
+
+        RadioButton rbCubiertoSi = (RadioButton) findViewById(R.id.rb_cubierto_si);
+        RadioButton rbCubiertoNo = (RadioButton) findViewById(R.id.rb_cubierto_no);
+        Button btnProduccionSiguiente = (Button) findViewById(R.id.btn_produccion_siguiente);
+
+
+
 
     }
 }
